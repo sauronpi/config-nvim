deb0d78a86fe4dae2f57ab10a3a440318e404c49
sauronpi <sauronpi@outlook.com>
2025-10-09 14:56:15 +0800

    update

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -6,3 +6,13 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "c", -- c文件
+  callback = function()
+    vim.opt_local.tabstop = 4 -- 一个tab显示为4个空格
+    vim.opt_local.shiftwidth = 4 -- 自动缩进时使用4个空格
+    vim.opt_local.expandtab = true -- 使用空格代替tab
+    vim.opt.softtabstop = 4 -- 控制输入和删除时的「感知宽度」，按tab键等效输入4个空格
+  end,
+})
--- lua/config/options.lua
+++ lua/config/options.lua
@@ -3,8 +3,8 @@
 -- Add any additional options here
 
 -- 缩进设置：4个空格
-vim.opt.tabstop = 4 -- Tab键显示为4个空格宽度
-vim.opt.shiftwidth = 4 -- 自动缩进（如if/for块）的宽度为4
-vim.opt.expandtab = true -- 将输入的Tab键自动转为空格（避免混合缩进）
-vim.opt.softtabstop = 4 -- 插入模式下，按Tab键等效输入4个空格
-vim.opt.smartindent = true -- 开启智能缩进（根据语法自动调整缩进，如代码块）
+-- vim.opt.tabstop = 4 -- Tab键显示为4个空格宽度
+-- vim.opt.shiftwidth = 4 -- 自动缩进（如if/for块）的宽度为4
+-- vim.opt.expandtab = true -- 将输入的Tab键自动转为空格（避免混合缩进）
+-- vim.opt.softtabstop = 4 -- 控制输入和删除时的「感知宽度」，按Tab键等效输入4个空格
+-- vim.opt.smartindent = true -- 开启智能缩进（根据语法自动调整缩进，如代码块）
