286cac7c5619139af256ae9afbfc119fe6018b07
sauronpi <sauronpi@outlook.com>
2026-04-15 03:06:00 +0800

    update

---
+++ lua/plugins/colorsechme.lua
@@ -0,0 +1,19 @@
+return {
+  -- add monokai-pro
+  {
+    "loctvl842/monokai-pro.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require("monokai-pro").setup()
+      vim.cmd.colorscheme("monokai-pro")
+    end,
+  },
+  -- Configure LazyVim to load gruvbox
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "monokai-pro",
+    },
+  },
+}
